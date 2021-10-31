04cdf0e63802180d5de172ea7abf2f88d897ec61
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-10-31 16:11:10 +0300

    first commit

---
+++ EmojiTable/EmojiTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiTable
+//
+//  Created by Yaroslav Monastyrev on 31.10.2021.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+
+}
