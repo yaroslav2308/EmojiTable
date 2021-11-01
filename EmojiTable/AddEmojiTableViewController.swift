e909ddbcd296066b558f653242554509d14dd583
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-02 01:14:25 +0300

    final commit

---
+++ EmojiTable/AddEmojiTableViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AddEmojiTableViewController.swift
+//  EmojiTable
+//
+//  Created by Yaroslav Monastyrev on 01.11.2021.
+//
+
+import UIKit
+
+class AddEmojiTableViewController: UITableViewController {
+    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
+    
+    @IBOutlet weak var emojiTF: UITextField!
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var descriptionTF: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateSabeButtonState()
+        undateUI()
+        navigationController?.navigationBar.barTintColor = UIColor.white
+    }
+    
+    private func updateSabeButtonState() {
+        let emojiText = emojiTF.text ?? ""
+        let nameText = nameTF.text ?? ""
+        let descriptionText = descriptionTF.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    private func undateUI() {
+        emojiTF.text = emoji.emoji
+        nameTF.text = emoji.name
+        descriptionTF.text = emoji.description
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        updateSabeButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        
+        let emoji = emojiTF.text ?? ""
+        let name = nameTF.text ?? ""
+        let description = descriptionTF.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
+        
+    }
+}
--- EmojiTable/EmojiTableViewController.swift
+++ EmojiTable/EmojiTableViewController.swift
@@ -14,7 +14,7 @@ class EmojiTableViewController: UITableViewController {
         Emoji(emoji: "🥥", name: "Cocoa", description: "keep", isFavourite: false),
         Emoji(emoji: "🐱", name: "Cat", description: "Lol", isFavourite: false)
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Emoji Reader"
@@ -22,19 +22,51 @@ class EmojiTableViewController: UITableViewController {
         self.navigationItem.leftBarButtonItem = self.editButtonItem
         
     }
-
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        guard segue.identifier == "saveSegue" else { return }
+        let sourceVC = segue.source as! AddEmojiTableViewController
+        let emoji = sourceVC.emoji
+        
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            objects[selectedIndexPath.row] = emoji
+            tableView.reloadRows(at: [selectedIndexPath], with: .fade)
+        } else {
+            let newIndexPath = IndexPath(row: objects.count, section: 0)
+            objects.append(emoji)
+            tableView.insertRows(at: [newIndexPath], with: .fade)
+        }
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "editEmoji" else { return }
+        
+        let indexPath = tableView.indexPathForSelectedRow!
+        let emoji = objects[indexPath.row]
+        
+        let navigationVC = segue.destination as! UINavigationController
+        let addEmojiVC = navigationVC.topViewController as! AddEmojiTableViewController
+        
+        addEmojiVC.emoji = emoji
+        addEmojiVC.title = "Edit"
+        
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return objects.count
     }
-
+    
     // Заполнение таблицы
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
@@ -96,50 +128,50 @@ class EmojiTableViewController: UITableViewController {
         action.image = UIImage(systemName: "heart")
         return action
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
